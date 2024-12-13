4df1b7b522bdbf681d29c006b71ebd8f6c2f1cae
daffalandra <mdaf.alandra@gmail.com>
2024-12-13 23:22:05 +0700

    initial commit

--- resources/js/Pages/event/index.vue
+++ resources/js/Pages/event/index.vue
@@ -11,7 +11,7 @@ import { Head } from '@inertiajs/vue3';
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Terseeart
+                Event
             </h2>
         </template>
 
